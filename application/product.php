3e50d19aae22ae2dd124b044de421b98156af43b
Mellan <mellanmalinis18@gmail.com>
2020-03-06 21:21:15 +0800

    delete product

--- application/product.php
+++ application/product.php
@@ -29,3 +29,52 @@ if (isset($_POST['add'])) {
 }
 
 
+
+
+// Update Product
+if (isset($_POST['update'])) { 
+  $product_code = $_GET['id']; 
+  $description = mysqli_real_escape_string($db, $_POST['description']); 
+  $price = mysqli_real_escape_string($db, $_POST['price']); 
+  $unit = mysqli_real_escape_string($db, $_POST['unit']); 
+
+  $sql = 'UPDATE product SET description="'.$description.'",price="'.$price.'",unit="'.$unit.'" WHERE product_code = '. $product_code;
+  $update = mysqli_query($db, $sql);
+  if($update){  
+    $_SESSION['error'] = '
+      <div class="alert alert-primary" role="alert">
+        <strong>New Record Updated!</strong>
+      </div>
+    '; 
+    header('location: ../edit_product.php?id=' . $product_code);
+  }else{ 
+    $_SESSION['error'] = '
+      <div class="alert alert-danger" role="alert">
+        <strong>Error.</strong>
+      </div>
+    ';  
+    header('location: ../edit_product.php?id=' . $product_code);
+  }  
+}
+
+// Delete product 
+if (isset($_GET['delete_product'])) { 
+  $product_code = $_GET['delete_product'];   
+  $sql = 'DELETE FROM product WHERE product_code=' . $product_code;
+  $delete = mysqli_query($db, $sql);
+  if($delete){  
+    $_SESSION['error'] = '
+      <div class="alert alert-primary" role="alert">
+        <strong>Record Successfully Deleted!</strong>
+      </div>
+    '; 
+    header('location: ../product.php');
+  }else{ 
+    $_SESSION['error'] = '
+      <div class="alert alert-danger" role="alert">
+        <strong>Data cannot be deleted, it is used in the other operation.</strong>
+      </div>
+    ';  
+    header('location: ../product.php');
+  }  
+}
