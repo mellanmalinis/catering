2425f87d90a4d14250488531a937869b48240a6d
Mellan <mellanmalinis18@gmail.com>
2020-03-06 11:20:55 +0800

    sidebar

---
+++ template/sidebar.php
@@ -0,0 +1,35 @@
+
+     <nav id="sidebar" class="sidebar-wrapper">
+       <div class="sidebar-content">
+         <div class="sidebar-brand">
+           <a href="#"></a>
+           <div id="close-sidebar">
+             <i class="fas fa-times"></i>
+           </div>
+         </div>
+         <div class="sidebar-header">
+         <img width="200" class="img-responsive img-rounded" src="assets/img/image.jpg" alt="User picture">
+            
+         </div>  
+         <div class="sidebar-menu">
+           <ul>
+             <li class="header-menu">
+               <span>Menu</span>
+             </li>
+             <li>
+               <a href="#">
+                 <i class="fa fa-tachometer-alt"></i>
+                 <span>Dashboard</span>
+               </a>
+             </li>   
+           </ul>
+         </div>
+         <!-- sidebar-menu  -->
+       </div>
+       <!-- sidebar-content  -->
+       <div class="sidebar-footer bg-danger"> 
+         <a href="#">
+           <i class="fa fa-power-off"></i>
+         </a>
+       </div>
+     </nav>
