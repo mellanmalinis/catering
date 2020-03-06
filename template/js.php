04d95a695cb7511edb808fcaf625171b2e21c4b8
Mellan <mellanmalinis18@gmail.com>
2020-03-06 14:06:26 +0800

    add catering, popper script source

--- template/js.php
+++ template/js.php
@@ -1,5 +1,8 @@
 
 
   <script src="assets/jquery.min.js"></script>
+  <script src="assets/bootstrap/js/popper.min.js"></script>
   <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/js/catering.js"></script>
 
+  
