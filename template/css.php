b91a7c60ff1f7e5080417837585c6e45ddd82bd2
Mellan <mellanmalinis18@gmail.com>
2020-03-06 14:06:05 +0800

    add catering stylesheet

--- template/css.php
+++ template/css.php
@@ -1,4 +1,6 @@
 
   <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css"> 
   <link rel="stylesheet" href="assets/fontawesome-free/css/all.min.css"> 
+  <link rel="stylesheet" href="assets/css/catering.css"> 
+  
   
