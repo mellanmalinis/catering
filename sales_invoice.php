aa5a20d5cd1b3214986523520f24f7903f5bcdab
Mellan <mellanmalinis18@gmail.com>
2020-03-06 22:28:20 +0800

    display form

--- sales_invoice.php
+++ sales_invoice.php
@@ -4,35 +4,119 @@
   if(!isset($_SESSION["id"])){ 
     header('location: login.php');
   }
-  $page_title = "Sales Invoce"; 
+  $page_title = "Sales Invoce";  
+  require_once 'application/config.php';
 ?>
- <!DOCTYPE html>
- <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 
- <head>
-   <?php require_once 'template/meta-info.php'; ?>
-   <?php require_once 'template/css.php'; ?> 
- 
- </head>
+<head>
+  <?php require_once 'template/meta-info.php'; ?>
+  <?php require_once 'template/css.php'; ?>
 
- <body>
-   <div class="page-wrapper chiller-theme toggled">
-     <a id="show-sidebar" class="btn btn-sm btn-dark" href="#">
-       <i class="fas fa-bars"></i>
-     </a>
-   <?php require_once 'template/sidebar.php'; ?> 
-     <!-- sidebar-wrapper  -->
-     <main class="page-content">
-       <div class="container-fluid">
-         <h2>Sales Invoce</h2>
-         <hr> 
-       </div>
+</head>
 
-     </main>
-     <!-- page-content" -->
-   </div>
+<body>
+  <div class="page-wrapper chiller-theme toggled">
+    <a id="show-sidebar" class="btn btn-sm btn-dark" href="#">
+      <i class="fas fa-bars"></i>
+    </a>
+    <?php require_once 'template/sidebar.php'; ?>
+    <!-- sidebar-wrapper  -->
+    <main class="page-content">
+      <div class="container-fluid">
+        <h2>Sales Invoce</h2>
+        <hr>
+
+        <form method="POST" id="sales-invoice">
+          <div class="row justify-content-center">
+            <div class="col-sm-4">
+              <div class="form-group">
+                <label for="">Customer</label>
+                <select name="customer" required="" class="form-control ">
+                  <option value="">Select</option>
+                  <?php  
+                $sql= "SELECT * FROM customer";
+                $result = mysqli_query($db, $sql);
+								if(mysqli_num_rows($result)){
+									while($row = mysqli_fetch_array($result)){
+							?>
+                  <option value="<?php echo $row['customer_id']; ?>"><?php echo $row['first_name'].' '.$row['last_name'] ?></option>
+                  <?php 
+									}
+								} 
+							?>
+                </select>
+              </div>
+              <div class="form-group">
+                <label for="">Term</label>
+                <select name="terms" required="" class="form-control ">
+                  <option value="">Select</option>
+                  <option value="Cash On Delivery">Cash On Delivery</option>
+                  <option value="Cash">Cash</option>
+                  <option value="Credit">Credit</option>
+                </select>
+              </div>
+              <div class="form-group">
+                <label for="">Product</label>
+                <select name="products" id="products" required="" class="form-control  ">
+                  <option value="">Select</option>
+                  <?php 
+								$query = "SELECT * FROM product";
+								$result1 = mysqli_query($db, $query);
+								if(mysqli_num_rows($result1)){
+									while($row = mysqli_fetch_array($result1)){
+							?>
+                  <option value="<?php echo $row['product_code']; ?>"><?php echo $row['description'];?></option>
+                  <?php 
+									}
+								} 
+							?>
+                </select>
+              </div>
+            </div>
+          </div>
+          <hr>
+          <div class="row">
+            <div class="col-sm-12">
+              <div class="table-responsive">
+                <table id="invoice-item-table" class="table table-bordered table-sm">
+                  <tr>
+                    <th>Productcode</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                    <th>Unit</th>
+                    <th rowspan="2"><button type="button" name="add_row" id="add_row" class="my-3 btn btn-block btn-success btn-sm btn-xs">ADD </button></th>
+                  </tr>
+                  <tr>
+                    <td><input type="text" name="products[]" id="barcode1" class="form-control form-control-sm input-sm barcode" placeholder="Barcode"/ required></td>
+                    <td><input type="number" min="1" name="quantity[]" id="quantity1" data-srno="1" class="form-control form-control-sm input-sm quantity" placeholder="Qty" required=""> </td>
+                    <td><input type="number" step="0.01" min="0.00" name="price[]" id="buy_price1" data-srno="1" class="form-control form-control-sm input-sm buy_price" placeholder="Price" required="'"> </td>
+                    <td><input type="text" name="unit[]" id="unit1" data-srno="1" class="form-control form-control-sm  input-sm unit" pattern="[A-Za-z0-9]+" placeholder="Unit" required></td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+          </div>
+          <div class="row justify-content-end">
+            <div class="col-sm-4">
+              <button type="submit" name="create_delivery" class="btn btn-primary btn-block">Okay</button>
+            </div>
+          </div>
+
+        </form>
+
+      </div>
+
+    </main>
+    <!-- page-content" -->
+  </div>
   <?php require_once 'template/js.php'; ?>
 
- </body>
 
- </html>
+</body>
+
+</html>
+<?php
+  unset($_SESSION["error"]);
+?>
