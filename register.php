d5565c80b78b29a608e3680694ad4f2055d740bb
Mellan <mellanmalinis18@gmail.com>
2020-03-06 15:53:41 +0800

    add fields

--- register.php
+++ register.php
@@ -31,11 +31,23 @@
       </div>
     </div>
     <div class="mt-5 row justify-content-center">
-      <div class="col-sm-4">
-        <form ethod="post" action="">
+      <div class="col-sm-4"> 
+        <form method="post" action="application/register.php">
+          <div class="form-group">
+            <label for="first_name" class="text-white">First Name</label>
+            <input type="text" name="first_name" class="form-control" placeholder="Firs Name" required="">
+          </div>
+          <div class="form-group">
+            <label for="mi" class="text-white">M.I.</label>
+            <input type="text" name="mi" class="form-control" placeholder="M.I." required="">
+          </div>
+          <div class="form-group">
+            <label for="last_name" class="text-white">Last Name</label>
+            <input type="text" name="last_name" class="form-control" placeholder="Last Name" required="">
+          </div>
           <div class="form-group">
             <label for="email" class="text-white">Email</label>
-            <input type="email" class="form-control" placeholder="Email" required="">
+            <input type="email"  name="email" class="form-control" placeholder="Email" required="">
           </div>
           <div class="form-group">
             <label for="username" class="text-white">Username</label>
@@ -45,13 +57,13 @@
             <label for="password" class="text-white">Password</label>
             <input type="password" name="password" class="form-control" placeholder="Password" required="">
           </div>
-          <button class="btn mt-5 btn-danger btn-block" type="submit" name="submit" value="Submit">Register</button>
+          <button class="btn mt-5 btn-danger btn-block" type="submit" name="register">Register</button>
         </form>
-        <p class="mt-5 text-white">Already have an account?  <a href="login.php"><u>login here.</u></a></p>
+        <p class="mt-5 text-white">Already have an account?  <a href="login.php"><u>Login here.</u></a></p>
       </div>
     </div>
   </div>
   <?php require_once 'template/js.php'; ?>
 </body>
 
-</html>
+</html> 
