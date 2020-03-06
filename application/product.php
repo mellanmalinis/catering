40fe02b494654fbb119b573436b60b36e83550de
Mellan <mellanmalinis18@gmail.com>
2020-03-06 21:05:32 +0800

    add product

---
+++ application/product.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+require_once 'config.php'; 
+
+// Add Proudct
+if (isset($_POST['add'])) { 
+  $description = mysqli_real_escape_string($db, $_POST['description']); 
+  $price = mysqli_real_escape_string($db, $_POST['price']); 
+  $quantity = mysqli_real_escape_string($db, $_POST['quantity']);   
+
+  // insert to produt  
+  $sql = 'INSERT INTO product (description, price, unit) VALUES ("'.$description.'", "'.$price.'", "'.$quantity.'")';
+  $insert = mysqli_query($db, $sql); 
+  if($insert){  
+    $_SESSION['error'] = '
+      <div class="alert alert-primary" role="alert">
+        <strong>New Record Added!</strong>
+      </div>
+    '; 
+    header('location: ../add_product.php');
+  }else{ 
+    $_SESSION['error'] = '
+      <div class="alert alert-danger" role="alert">
+        <strong>Error.</strong>
+      </div>
+    ';  
+    header('location: ../add_product.php');
+  }  
+}
+
